19e5e05d7b68dc64345a78b3e3d798cd01389911
kikuchit <takashi.kikuchi.0914@gmail.com>
2015-07-21 18:03:51 +0900

    初回コミット

---
+++ TCPSender/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TCPSender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(TCPSender sender = new TCPSender("127.0.0.1", 15000))
+            {
+                string message = "hogehoge";
+                byte[] data = Encoding.UTF8.GetBytes(message);
+                sender.Connect();
+                Console.WriteLine("Write data:{0}", message);
+                sender.Write(data);
+            }
+        }
+    }
+}
---
+++ TCPSender/TCPSender.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Sockets;
+
+namespace TCPSender
+{
+    class TCPSender : IDisposable
+    {
+        string server;
+        int port;
+        TcpClient client;
+        NetworkStream stream;
+
+        public void Dispose()
+        {
+            Console.WriteLine("TCPSender:disposing");
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if(disposing)
+            {
+                Console.WriteLine("TCPSender:managed resource disposing");
+
+                if (this.client != null)
+                {
+                    this.client.Close();
+                }
+
+                if (this.stream != null)
+                {
+                    this.stream.Dispose();
+                }
+            }
+        }
+
+        ~TCPSender()
+        {
+            Console.WriteLine("TCPSender:destructor");
+            Dispose(false);
+        }
+
+        public TCPSender(string server, int port)
+        {
+            this.server = server;
+            this.port = port;
+            this.client = null;
+            this.stream = null;
+        }
+
+        public void Connect()
+        {
+            try
+            {
+                client = new TcpClient(server, port);
+                stream = client.GetStream();
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void Write(byte[] sendData)
+        {
+            if(this.stream != null && this.stream.CanWrite)
+            {
+                this.stream.Write(sendData, 0, sendData.Length);
+            }
+            else
+            {
+                // TODO:適当な例外にすべき。
+                throw new Exception();
+            }
+        }
+    }
+}
